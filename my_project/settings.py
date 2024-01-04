8139020a3154fc80d24e8aac726ef17dd24688ac
Penny Train <penny.train04@gmail.com>
2024-01-04 13:08:18 +0000

    Allowed hosts = True

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=cu(eo3z^33d(p5_4tvq!q!cii+)h#883vcc!6@s9tq%)1^g5l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
